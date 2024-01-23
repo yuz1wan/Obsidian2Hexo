a686fda4ad15c2468239b9d861c17bea3a053f9c
Ziyu Wang <96973724+yuz1wan@users.noreply.github.com>
2024-01-23 10:38:41 +0800

    Update push_hexoblog.sh

--- push_hexoblog.sh
+++ push_hexoblog.sh
@@ -1,3 +1,4 @@
+K=$1
 cd HexoBlog-Resp
 git config --global user.email "1172789766@qq.com"
 git config --global user.name "Wang Ziyu"
@@ -5,4 +6,4 @@ git add .
 current="`date +'%Y-%m-%d %H:%M:%S'`"
 msg="Updated: $current"
 git commit -m "$msg"
-git push -u origin master:main -f
+git push origin master:main -f -u ${K}
